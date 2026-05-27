3bca9b835f78e2c85b86096a8ded1b9e537262e7
23f3002451 <23f3002451@ds.study.iitm.ac.in>
2026-05-27 12:33:39 +0530

    Initial commit

---
+++ api/index.py
@@ -0,0 +1,47 @@
+import math
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel,List
+import json
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=['GET','POST','PUT','DELETE'],
+    allow_credentials=True,
+    allow_headers=["*"],
+)
+class Payload(BaseModel):
+    regions: List[str]
+    threshold_ms: int
+with open('/q-vercel-latency.json') as file:
+    data=json.load(file)
+@app.post("/api/latency")
+async def analyze(payload:Payload):
+    result={}
+    for region in payload.regions:
+        count=0
+        uptimes=[]
+        latencies=[]
+        for i in range(len(data)):
+            if(data[i].region==region):
+                uptimes.append(data[i].uptime_pct)
+                latencies.append(data[i].latency_ms)
+            if(data[i].latency_ms>payload.threshold_ms):
+                count+=1
+            else:
+                continue
+        avg_latency=sum(latencies)/len(latencies)
+        sorted_latencies=sorted(latencies)
+        index = math.ceil(0.95 * len(sorted_latencies)) - 1
+        p95_latency = sorted_latencies[index]
+        avg_uptime= sum(uptimes)/len(uptimes)
+        result[region]={
+        'avg_latency':round(avg_latency,2),
+        'p95_latency':round(p95_latency,2),
+        'avg_uptime':round(avg_uptime,3),
+        'breaches':count
+        }
+    return(result)
+
+       
